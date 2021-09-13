841fe04ed39dd19d7f92a7ee0daa7c4e29a262f8
bertdelaspeed <bertdelaspeed@gmail.com>
2021-09-13 15:57:20 +0000

    django heroku

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -191,3 +194,5 @@ MEDIA_ROOT = 'static/images'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
